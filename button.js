6b3a2143e46bf564c8a7c2858ca8fc0c1c53e63e
kvakash <72356683+kvakash@users.noreply.github.com>
2022-02-03 11:25:45 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,7 @@
+import React, { Component }  from 'react';
+
+
+const Button = ({ color, text, onClick }) => {
+    return <button onClick={onClick} style={{ backgroundColor: color }} className="btn">{text}</button>
+  }
+  export default Button;
---
+++ header.js
@@ -0,0 +1,12 @@
+import React, { Component }  from 'react';
+import Button from '../CRUD/button';
+import "../index.css";
+const Header = ({ showForm, changeTextAndColor }) => {
+  return (
+      <header className="header">
+          <h2 className="app-header">Task Manager App</h2>
+          <Button onClick={showForm} color={changeTextAndColor ? 'red' : 'green'} text=     {changeTextAndColor ? 'Close' : 'Add'} />
+        </header>
+    )
+}
+export default Header;
